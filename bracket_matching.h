42fda83bd23a5e6f2cfe44704301e3c3b32f898e
sherwin194 <vazsherwin71@gmail.com>
2023-10-03 21:50:56 +0530

    committed bracket_matching.h

---
+++ bracket_matching.h
@@ -0,0 +1,10 @@
+// bracket_matching.h
+
+#ifndef BRACKET_MATCHING_H
+#define BRACKET_MATCHING_H
+
+#include <stdbool.h>
+
+bool areBracketsMatched(const char *expression);
+
+#endif // BRACKET_MATCHING_H
