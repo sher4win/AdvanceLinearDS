2e16698329c0d8dc11b9628b33a0e1a2f59ab359
sherwin194 <vazsherwin71@gmail.com>
2023-10-03 21:45:01 +0530

    committed infix_to_postfix.h

---
+++ infix_to_postfix.h
@@ -0,0 +1,10 @@
+// infix_to_postfix.h
+
+#ifndef INFIX_TO_POSTFIX_H
+#define INFIX_TO_POSTFIX_H
+
+#include "stack.h"
+
+void infixToPostfix(const char *infixExpression, char *postfixExpression);
+
+#endif // INFIX_TO_POSTFIX_H
